0a387993e9e4b21fb0d413657cb5a98a6df5ec30
huesihues <joanhuesoaguilar@gmail.com>
2015-09-23 18:19:41 +0200

    He añadido el proyecto entero xD

---
+++ ProyectoGod2/Assets/Scripts/Player/UserInput2.cs
@@ -0,0 +1,67 @@
+﻿using RTS;
+using UnityEngine;
+using System.Collections;
+
+	
+public class UserInput2 : MonoBehaviour {
+
+	private Player player;
+
+	Vector3 movement = new Vector3 (0,0,0);
+
+	// Use this for initialization
+	void Start () {
+		player = transform.root.GetComponent <Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.human) {
+			MoveCamera ();
+			RotateCamera ();
+		}
+	}
+
+	private void MoveCamera(){
+		Vector3 origin = Camera.main.transform.position;
+		Vector3 destination = origin;
+		float xpos = Input.mousePosition.x;
+		float ypos = Input.mousePosition.y;
+		//movimiento horizontal de la camara
+		if (xpos >= 0 && xpos < ResourceManager.ScrollWidth) {
+			movement.x -= ResourceManager.ScrollSpeed;
+		} else if (xpos <= Screen.width && xpos > Screen.width - ResourceManager.ScrollWidth) {
+			movement.x += ResourceManager.ScrollSpeed;
+		}
+		//movimiento vertical de la camara
+		if (ypos >= 0 && ypos < ResourceManager.ScrollWidth) {
+			movement.z -= ResourceManager.ScrollSpeed;
+		} else if (ypos <= Screen.width && ypos > Screen.width - ResourceManager.ScrollWidth) {
+			movement.z += ResourceManager.ScrollSpeed;
+		}
+
+		movement = Camera.main.transform.TransformDirection (movement);
+		movement.y = 0;
+		movement.y -= ResourceManager.ScrollSpeed * Input.GetAxis ("Mouse ScrollWheel");
+
+		destination.x += movement.x;
+		destination.y += movement.y;
+		destination.z += movement.z;
+
+		if (destination.y > ResourceManager.MaxCameraHeight) {
+			destination.y = ResourceManager.MaxCameraHeight;
+		} else if (destination.y < ResourceManager.MinCameraHeight) {
+			destination.y = ResourceManager.MinCameraHeight;
+		}
+
+		if (destination != origin){
+			Camera.main.transform.position = Vector3.MoveTowards(origin, destination, Time.deltaTime * ResourceManager.ScrollSpeed);
+		}
+
+
+	}
+
+	private void RotateCamera(){
+
+	}
+}
---
+++ ProyectoGod2/Assets/Scripts/RTS/ResourceManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RTS {
+	
+	public static class ResourceManager {
+
+
+		public static float ScrollSpeed { 
+			get { 
+				return 15f;
+			} 
+		}
+		public static float RotateSpeed { 
+			get {
+				return 100; 
+			} 
+		}
+		public static int ScrollWidth {
+			get {
+				return 50;
+			}
+		}
+		public static float RotateAmount { get { return 15; } }
+		public static float MinCameraHeight {
+			get {
+				return 6;
+			}
+		}
+		public static float MaxCameraHeight {
+			get {
+				return 10;
+			}
+		}
+
+	}
+}
